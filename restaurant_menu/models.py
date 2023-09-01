abb17abe51e0cbfffab295d4bc9aed2c8620ede8
abduyus <yusuf.abdurrasheed@outlook.com>
2023-09-01 19:16:00 +0000

    creating class based vies and implementing urls

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -15,7 +15,7 @@ STATUS = (
 )
 
 
-class Items(models.Model):
+class Item(models.Model):
     meal = models.CharField(max_length=1000, unique=True)
     description = models.CharField(max_length=2000)
     price = models.DecimalField(decimal_places=2, max_digits=10)
